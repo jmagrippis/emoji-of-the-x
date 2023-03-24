35f7244f1af99bf5dd26ff7af53119bb3fbcbb38
Johnny Magrippis <j@magrippis.com>
2023-03-24 19:52:52 +0100

    ♻️  use node-html-parser so we can deploy on the EDGE

--- src/routes/api/scrape/reduceEmojis.ts
+++ src/routes/api/scrape/reduceEmojis.ts
@@ -1,20 +1,20 @@
-import {Window} from 'happy-dom'
+import {parse} from 'node-html-parser'
+
 type Emoji = {
 	code: string
 	character: string
 	name: string
 }
 export const reduceEmojis = (html: string) => {
-	const window = new Window()
-	window.document.body.innerHTML = html
-	const emojiTableRows = window.document.querySelectorAll('tr')
+	const root = parse(html)
+
+	const emojiTableRows = root.querySelectorAll('tr')
 
 	const emojiRows = emojiTableRows.reduce<Emoji[]>((acc, row) => {
-		const isItEmojiRow = !!row.querySelector('td.code')
-		if (isItEmojiRow) {
-			const code = row.querySelector('td.code').textContent
-			const character = row.querySelector('td.chars').textContent
-			const name = row.querySelector('td.name').textContent
+		const code = row.querySelector('td.code')?.textContent
+		const character = row.querySelector('td.chars')?.textContent
+		const name = row.querySelector('td.name')?.textContent
+		if (code && character && name) {
 			acc.push({code, character, name})
 		}
 		return acc
