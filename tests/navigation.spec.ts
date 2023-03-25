f6f794286388ceb507367da589cb878047ede194
Johnny Magrippis <j@magrippis.com>
2023-03-25 20:25:26 +0100

    ⬆️  upgrade node version + deps

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -7,6 +7,12 @@ test('navigation smoke test', async ({page}) => {
 	await expect(page).toHaveTitle(/Emoji of the/i)
 	await expect(page.locator('role=heading[level=1]')).toHaveText('Emoji of the...')
 
+	// navigate to the Advice Page
+	await page.locator('role=link[name=Advice]').click()
+
+	await expect(page).toHaveTitle(/How would X make content about/)
+	await expect(page.locator('role=heading[level=1]')).toHaveText('Content Creation Advice')
+
 	// navigate to the About Page
 	await page.locator('role=link[name=About]').click()
 
